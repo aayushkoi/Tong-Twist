3b20f5a8a9c0d7ace2b5e24442df847788630a17
mkulker <mkulker@umass.edu>
2025-02-20 17:09:54 -0500

    fixed Navbar

--- tong/src/app/layout.js
+++ tong/src/app/layout.js
@@ -1,6 +1,6 @@
 // tong/src/app/layout.jsx
 import { AuthProvider } from "../context/AuthProvider";
-import Navbar from "../app/components/navbar";
+import Navbar from "../app/components/Navbar";
 import "../globals.css";
 
 export default function RootLayout({ children }) {
